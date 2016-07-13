cff9f123985b59d430320a5e74ff8aa7f83e4d25
mayu <1161484497@qq.com>
2016-07-13 15:33:04 +0800

    test
    test

--- Android/BrickMan/app/src/main/java/com/brickman/app/ui/main/LoginActivity.java
+++ Android/BrickMan/app/src/main/java/com/brickman/app/ui/main/LoginActivity.java
@@ -38,7 +38,7 @@ import static android.Manifest.permission.READ_CONTACTS;
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
-    
+
     /**
      * Id to identity READ_CONTACTS permission request.
      */
