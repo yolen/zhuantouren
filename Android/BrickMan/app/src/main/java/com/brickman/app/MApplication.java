94ca091d0624a58855dc90fe93208bfd52cac0ea
mayu <1161484497@qq.com>
2016-07-14 11:02:39 +0800

    LoadingDialog

---
+++ Android/BrickMan/app/src/main/java/com/brickman/app/MApplication.java
@@ -0,0 +1,13 @@
+package com.brickman.app;
+
+import android.app.Application;
+
+/**
+ * Created by mayu on 16/7/14,上午9:56.
+ */
+public class MApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- Android/BrickMan/app/src/main/java/com/brickman/app/ui/BaseActivity.java
+++ Android/BrickMan/app/src/main/java/com/brickman/app/ui/BaseActivity.java
@@ -1,12 +1,45 @@
 package com.brickman.app.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class BaseActivity extends AppCompatActivity {
+import com.brickman.app.MApplication;
+import com.brickman.app.R;
+import com.brickman.app.ui.dialog.LoadingDialog;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
+public abstract class BaseActivity extends AppCompatActivity {
+    protected MApplication mApp;
+    protected LoadingDialog mLoadingDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 设置状态栏颜色
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            setTranslucentStatus(true);
+        }
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        tintManager.setStatusBarTintResource(R.color.colorPrimaryDark);//通知栏所需颜色
+
+        mApp = (MApplication) getApplication();
+        mLoadingDialog = new LoadingDialog(this);
+    }
+
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
     }
 }
