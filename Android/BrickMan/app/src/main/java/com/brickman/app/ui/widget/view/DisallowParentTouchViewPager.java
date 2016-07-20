6028387b6f6e221d25f3708dbadae2ad6bff4812
mayu <1161484497@qq.com>
2016-07-20 10:29:49 +0800

    基础框架

---
+++ Android/BrickMan/app/src/main/java/com/brickman/app/ui/widget/view/DisallowParentTouchViewPager.java
@@ -0,0 +1,54 @@
+package com.brickman.app.ui.widget.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+
+
+
+/**
+ * Created by Denny on 2015/8/1.
+ */
+public class DisallowParentTouchViewPager extends ViewPager {
+
+    private ViewGroup parent;
+
+    public DisallowParentTouchViewPager(Context context) {
+        super(context);
+    }
+
+    public DisallowParentTouchViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setNestParent(ViewGroup parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (parent != null) {
+            parent.requestDisallowInterceptTouchEvent(true);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (parent != null) {
+            parent.requestDisallowInterceptTouchEvent(true);
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (parent != null) {
+            parent.requestDisallowInterceptTouchEvent(true);
+        }
+        return super.onTouchEvent(ev);
+    }
+}
+
