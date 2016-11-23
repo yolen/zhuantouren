722c97a260664e1122d91d03dacbef14a2d3c0f1
mayu <1161484497@qq.com>
2016-11-23 10:16:17 +0800

    解决app启动延迟问题

--- Android/BrickMan/app/src/main/java/com/brickman/app/MApplication.java
+++ Android/BrickMan/app/src/main/java/com/brickman/app/MApplication.java
@@ -5,10 +5,8 @@ import android.app.Application;
 import android.content.ComponentName;
 import android.content.Context;
 import android.os.Environment;
-import android.util.Log;
 
 import com.brickman.app.common.data.DataKeeper;
-import com.brickman.app.common.data.cipher.Base64Cipher;
 import com.brickman.app.common.glide.GlideImageLoader;
 import com.brickman.app.common.glide.GlidePauseOnScrollListener;
 import com.brickman.app.model.Bean.UserBean;
@@ -27,78 +25,65 @@ import cn.finalteam.galleryfinal.ThemeConfig;
  */
 public class MApplication extends Application {
 
-    /** 首先默认个文件保存路径 */
-    public static final String SAVE_PATH= Environment.getExternalStorageState().equalsIgnoreCase(Environment.MEDIA_MOUNTED) ? Environment.getExternalStorageDirectory().getAbsolutePath() : "/mnt/sdcard";//保存到SD卡
-    public static final String SAVE_PIC_PATH = SAVE_PATH+ "/brickman/savePic";//保存的确切位置
+    /**
+     * 首先默认个文件保存路径
+     */
+    public static final String SAVE_PATH = Environment.getExternalStorageState().equalsIgnoreCase(Environment.MEDIA_MOUNTED) ? Environment.getExternalStorageDirectory().getAbsolutePath() : "/mnt/sdcard";//保存到SD卡
+    public static final String SAVE_PIC_PATH = SAVE_PATH + "/brickman/savePic";//保存的确切位置
 
     public static MApplication mAppContext;
     public static DataKeeper mDataKeeper;
     public UserBean mUser;
     public boolean isNight = false;
 
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
+    public void inite() {
+        Logger.init("BRICK_MAN");
+        mDataKeeper = new DataKeeper(mAppContext, "BRICK_MAN");
+        mAppContext.mUser = (UserBean) mDataKeeper.get("user_info");
+        com.yolanda.nohttp.Logger.setTag("NoHttpSample");
+        com.yolanda.nohttp.Logger.setDebug(true);// 开始NoHttp的调试模式, 这样就能看到请求过程和日志
+        NoHttp.initialize(mAppContext);
+        NoHttp.setEnableCache(true);
+        //设置主题
+        ThemeConfig theme = new ThemeConfig.Builder()
+                .setCheckNornalColor(mAppContext.getResources().getColor(R.color.light_gray))
+                .setCheckSelectedColor(mAppContext.getResources().getColor(R.color.dark_green))
+                .setCropControlColor(mAppContext.getResources().getColor(R.color.white))
+                .setTitleBarBgColor(mAppContext.getResources().getColor(R.color.colorAccent))
+                .setTitleBarTextColor(mAppContext.getResources().getColor(R.color.white))
+                .setTitleBarIconColor(mAppContext.getResources().getColor(R.color.white))
+                .setFabNornalColor(mAppContext.getResources().getColor(R.color.colorAccent))
+                .setFabPressedColor(mAppContext.getResources().getColor(R.color.colorPrimaryDark))
+                .build();
+        //配置功能
+        FunctionConfig functionConfig = new FunctionConfig.Builder()
+                .setEnableCamera(true)
+                .setEnableEdit(true)
+                .setEnableCrop(true)
+                .setEnableRotate(true)
+                .setCropSquare(true)
+                .setEnablePreview(true)
+                .build();
+        CoreConfig coreConfig = new CoreConfig.Builder(mAppContext, new GlideImageLoader(), theme)
+                .setFunctionConfig(functionConfig)
+                .setPauseOnScrollListener(new GlidePauseOnScrollListener(false, true))
+                .build();
+        GalleryFinal.init(coreConfig);
     }
 
-
-    public  void inite(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                //配置程序异常退出处理
-//        Thread.setDefaultUncaughtExceptionHandler(new LocalFileHandler(this));
-                Logger.init("BRICK_MAN");
-                com.yolanda.nohttp.Logger.setTag("NoHttpSample");
-                com.yolanda.nohttp.Logger.setDebug(true);// 开始NoHttp的调试模式, 这样就能看到请求过程和日志
-                NoHttp.initialize(mAppContext);
-                NoHttp.setEnableCache(true);
-                //设置主题
-                ThemeConfig theme = new ThemeConfig.Builder()
-                        .setCheckNornalColor(mAppContext.getResources().getColor(R.color.light_gray))
-                        .setCheckSelectedColor(mAppContext.getResources().getColor(R.color.dark_green))
-                        .setCropControlColor(mAppContext.getResources().getColor(R.color.white))
-                        .setTitleBarBgColor(mAppContext.getResources().getColor(R.color.colorAccent))
-                        .setTitleBarTextColor(mAppContext.getResources().getColor(R.color.white))
-                        .setTitleBarIconColor(mAppContext.getResources().getColor(R.color.white))
-                        .setFabNornalColor(mAppContext.getResources().getColor(R.color.colorAccent))
-                        .setFabPressedColor(mAppContext.getResources().getColor(R.color.colorPrimaryDark))
-                        .build();
-                //配置功能
-                FunctionConfig functionConfig = new FunctionConfig.Builder()
-                        .setEnableCamera(true)
-                        .setEnableEdit(true)
-                        .setEnableCrop(true)
-                        .setEnableRotate(true)
-                        .setCropSquare(true)
-                        .setEnablePreview(true)
-                        .build();
-                CoreConfig coreConfig = new CoreConfig.Builder(mAppContext, new GlideImageLoader(), theme)
-                        .setFunctionConfig(functionConfig)
-                        .setPauseOnScrollListener(new GlidePauseOnScrollListener(false, true))
-                        .build();
-                GalleryFinal.init(coreConfig);
-
-
-            }
-        }).start();
-
-    }
     @Override
     public void onCreate() {
         super.onCreate();
         mAppContext = this;
-        mDataKeeper = new DataKeeper(mAppContext, "BRICK_MAN");
-        mAppContext.mUser = (UserBean) mDataKeeper.get("user_info");
     }
 
-    public static MApplication getInstance(){
+    public static MApplication getInstance() {
         return mAppContext;
     }
 
     /**
      * 获取服务是否开启
+     *
      * @param className 完整包名的服务类名
      */
     public static boolean isRunningService(String className, Context context) {
@@ -117,16 +102,16 @@ public class MApplication extends Application {
         return false;
     }
 
-    public static void setLocation(String city, String address){
+    public static void setLocation(String city, String address) {
         mDataKeeper.put("city", city);
         mDataKeeper.put("address", address);
     }
 
-    public static String getCity(){
+    public static String getCity() {
         return mDataKeeper.get("city", "北京");
     }
 
-    public static String getAddress(){
+    public static String getAddress() {
         return mDataKeeper.get("address", "北京");
     }
 }
